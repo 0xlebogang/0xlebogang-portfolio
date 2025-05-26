64f9a54e845a10a440944d319ca3891f01f3d05a
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-05-26 15:47:15 +0200

    implement about section content config

--- apps/web/src/components/sections/about/compact/about.tsx
+++ apps/web/src/components/sections/about/compact/about.tsx
@@ -6,6 +6,8 @@ import MotionWrap from "@/components/motion-wrap";
 import { Button } from "@repo/ui/button";
 import { Icons } from "@repo/ui/icons";
 
+import { about } from "../config";
+
 function About() {
   return (
     <MotionWrap className="w-full py-24 lg:py-32" id="about">
@@ -13,18 +15,15 @@ function About() {
         <div className="grid gap-10 lg:grid-cols-2">
           <div className="space-y-4">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-              About Me
+              {about.title}
             </h2>
             <div className="space-y-4">
               <p className="max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                I am a passionate and creative Web Developer with a love for
-                beautiful and functional websites. I have experience working
-                with a variety of web technologies and frameworks and I am
-                always eager to learn new things and take on new challenges.
+                {about.description}
               </p>
               <div className="flex gap-2">
                 <Button asChild variant={"outline"}>
-                  <a href="/resume.pdf" target="_blank">
+                  <a href={about.resumeUrl} target="_blank">
                     View Resume <Icons.arrowUpRight className="ml-2 size-5" />
                   </a>
                 </Button>
@@ -36,10 +35,10 @@ function About() {
           </div>
           <div className="grid gap-4 sm:gap-6">
             <Image
-              alt="Image"
+              alt={about.image?.alt ?? "Image"}
               className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center"
               height="310"
-              src="/images/hero.jpg"
+              src={about.image?.imageUrl ?? "/images/hero.jpg"}
               sizes="100vw"
               width="550"
             />
