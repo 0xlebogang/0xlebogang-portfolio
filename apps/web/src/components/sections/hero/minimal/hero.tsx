4d47e3a88b109eb6ae64efca9fc973cdbe3766dd
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-05-26 12:45:56 +0200

    fix hero layout

--- apps/web/src/components/sections/hero/minimal/hero.tsx
+++ apps/web/src/components/sections/hero/minimal/hero.tsx
@@ -7,11 +7,11 @@ function Hero() {
     <MotionWrap className="flex h-[calc(100dvh-(--spacing(14)))] w-full items-center justify-center">
       <div className="flex w-full items-center justify-center gap-4 px-4 md:grid-cols-2 md:px-6 lg:gap-10">
         <div className="flex w-full items-center justify-center space-y-3 text-left">
-          <div className="flex w-full max-w-(--breakpoint-xl) flex-col items-center justify-center">
-            <h1 className="text-end text-8xl font-bold tracking-tight md:text-9xl lg:text-[14rem]">
+          <div className="flex w-full max-w-(--breakpoint-xl) flex-col items-center center">
+            <h1 className="text-end text-7xl sm:text-8xl font-bold tracking-tight md:text-9xl lg:text-[14rem]">
               {hero.name}
             </h1>
-            <div className="dark:bg-foreground/10 mb-4 inline-block w-fit rounded-lg bg-gray-100 px-3 py-1 text-sm">
+            <div className="text-xs dark:bg-foreground/10 mb-4 inline-block w-fit rounded-lg bg-gray-100 px-3 py-1 sm:text-sm ml-auto">
               {hero.label}
             </div>
           </div>
