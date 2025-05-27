47c4b03e7ddcd528697821154b1966036b004918
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-05-27 11:26:27 +0200

    update work experience content

--- apps/web/src/components/sections/experience/config.ts
+++ apps/web/src/components/sections/experience/config.ts
@@ -1,34 +1,19 @@
-import type { Experience } from "@/types/experience";
+import type { Experience, ExperienceContent } from "@/types/experience";
 
 const experiences: Experience[] = [
   {
-    name: "Web Development",
-    duration: "2022 - Present",
-    company: "Acme Inc",
+    name: "General Assistant",
+    duration: "2018 - 2021 (Part-time)",
+    company: "Thrupps & Co.",
     description:
-      "Building beautiful and functional websites using modern web technologies, focusing on performance, accessibility, and SEO-friendly design.",
-  },
-  {
-    name: "UI/UX Design",
-    duration: "2021 - 2022",
-    company: "Creative Designs Ltd",
-    description:
-      "Creating delightful and intuitive user experiences, specializing in responsive design and user-centric methodologies.",
-  },
-  {
-    name: "Database Management",
-    duration: "2020 - 2022",
-    company: "DataTech Solutions",
-    description:
-      "Storing and organizing data efficiently, ensuring data integrity, and optimizing database queries for high traffic applications.",
-  },
-  {
-    name: "Mobile Development",
-    duration: "2019 - 2021",
-    company: "Innovative Apps Inc",
-    description:
-      "Crafting engaging and scalable apps for smartphones and tablets, focusing on cross-platform development and native performance.",
+      "Assisted in various tasks including customer service, gift wrapping, and administrative support. Developed strong organizational skills and a creative eye.",
   },
 ];
 
-export { experiences };
+const content: ExperienceContent = {
+  title: "Experience",
+  description: "A brief overview of my professional journey and roles.",
+  experiences: experiences,
+};
+
+export { content };
